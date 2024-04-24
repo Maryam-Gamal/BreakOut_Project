3eac50ca0bdcc761bfdb19d5cc26f1bfde34f145
ghadasherif <135965326+ghadasherif@users.noreply.github.com>
2024-04-24 11:28:44 +0200

    ghadas commit

--- Block.h
+++ Block.h
@@ -1,4 +1,13 @@
 #ifndef BLOCK_H
 #define BLOCK_H
+#include <QGraphicsRectItem>
+#include <QGraphicsScene>
 
+class Block : public QGraphicsRectItem {
+public:
+    Block(QGraphicsItem* parent = nullptr, int row = 0); // Constructor
+
+private:
+    
+};
 #endif // BLOCK_H
