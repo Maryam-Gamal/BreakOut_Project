7ad059daa5f96310a99daea55bc2e3e147be8dba
mennaelsafty <159125325+mennaelsafty@users.noreply.github.com>
2024-04-24 12:30:43 +0200

    Update Ball.h

--- Ball.h
+++ Ball.h
@@ -1,4 +1,26 @@
+
 #ifndef BALL_H
 #define BALL_H
+
+#include <QGraphicsRectItem>
+#include <QObject>
+
+class Ball : public QObject, public QGraphicsRectItem {
+    Q_OBJECT
+public:
+    Ball(QGraphicsItem *parent = nullptr);
+    double getCenterX() const;
+
+public slots:
+    void move();
+
+private:
+    double x_velocity;
+    double y_velocity;
+
+    void reverseVelocity();
+    void handlePaddleCollision();
+    void handleBlockCollision();
+};
 
 #endif // BALL_H
